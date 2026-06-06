fad236f44e8365d27436d3cb368496efd5454a7f
leninjamk <brayannago98@gmail.com>
2026-06-06 00:14:27 -0300

    fix: improve error handling in audio playback method

--- core/audio_engine.py
+++ core/audio_engine.py
@@ -10,11 +10,14 @@ class AudioEngine:
         print(f"🎧 OUTPUT fixado: {index}")
 
     def play(self, file):
-        if self.output_device is None:
-            print("❌ sem saída definida")
-            return
+        try:
+            if self.output_device is None:
+                print("❌ sem saída definida")
+                return
 
-        data, sr = sf.read(file)
+            data, sr = sf.read(file)
 
-        sd.play(data, sr, device=self.output_device)
-        sd.wait()
+            sd.play(data, sr, device=self.output_device)
+            sd.wait()
+        except Exception as e:
+            print("❌ Audio Engine play error:", e)
