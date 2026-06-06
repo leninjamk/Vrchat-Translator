572e381d71ad3d4f78be5cef55d32fd1762197b3
leninjamk <brayannago98@gmail.com>
2026-06-05 23:46:51 -0300

    feat: update OSC client initialization to support configurable IP and port

--- core/osc.py
+++ core/osc.py
@@ -1,6 +1,12 @@
 from pythonosc import udp_client
 
-client = udp_client.SimpleUDPClient("127.0.0.1", 9000)
+try:
+    from config import OSC_IP, OSC_PORT
+except ImportError:
+    OSC_IP = "127.0.0.1"
+    OSC_PORT = 9000
+
+client = udp_client.SimpleUDPClient(OSC_IP, OSC_PORT)
 
 def send_chat(msg):
     client.send_message("/chatbox/input", [msg, True])
